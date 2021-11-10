4e9406430fee35f228b669326686174a3291158b
charldonboshoff <30074800@student.g.nwu.ac.za>
2021-11-10 18:40:47 +0200

    Gallery Controller

--- Controllers/GalleryController.cs
+++ Controllers/GalleryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ShareImageProject.Models;
+using SimpleImageGallery.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,62 @@ namespace ShareImageProject.Controllers
     {
         public IActionResult Index()
         {
+            var coffeeImageTags = new List<ImageTag>();
+            var gamingConsoleImageTags = new List<ImageTag>();
+
+            var tagOne = new ImageTag()
+            { 
+                Description = "Coffee freak",
+                Id = 0
+            };
+
+            var tagTwo = new ImageTag()
+            {
+                Description = "Xbox user",
+                Id = 1
+            };
+
+            var tagThree = new ImageTag()
+            {
+                Description = "Playstation user",
+                Id = 2
+            };
+
+            coffeeImageTags.Add(tagOne);
+            gamingConsoleImageTags.AddRange(new List<ImageTag> { tagTwo, tagThree});
+
+            var imageList = new List<GalleryImage>()
+            {
+                new GalleryImage()
+                {
+                    Title = "Coffee",
+                    Url = "",
+                    Created = DateTime.Now,
+                    Tags = coffeeImageTags
+                },
+
+                new GalleryImage()
+                {
+                    Title = "Xbox One",
+                    Url = "",
+                    Created = DateTime.Now,
+                    Tags = gamingConsoleImageTags
+                },
+
+                new GalleryImage()
+                {
+                    Title = "Playstation 4",
+                    Url = "",
+                    Created = DateTime.Now,
+                    Tags = gamingConsoleImageTags
+                }
+            };
+            
             var model = new GalleryIndexModel
             {
-            
+                Images = imageList
             };
+
             return View(model);
         }
     }
