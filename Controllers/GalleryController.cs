e92de8e295c869fed1c656e5dbfbe0fb2a49b1b9
charldonboshoff <30074800@student.g.nwu.ac.za>
2021-11-10 13:24:06 +0200

    Add project files.

---
+++ Controllers/GalleryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShareImageProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShareImageProject.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new GalleryIndexModel
+            {
+            
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Models/GalleryIndexModel.cs
@@ -0,0 +1,15 @@
+﻿using SimpleImageGallery.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShareImageProject.Models
+{
+    public class GalleryIndexModel
+    {
+        public IEnumerable<GalleryImage> Images { get; set; }
+        //Search query
+        public string SearchQuery { get; set; }
+    }
+}
