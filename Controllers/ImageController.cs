18097a754cf362cd2c8788dbb30e69f0eb34a682
charldonboshoff <30074800@student.g.nwu.ac.za>
2021-11-14 18:20:36 +0200

    Created several classes in site.css and implemented those classe in imageConroller, uploadImageModel and the detail and index view in gallery. Also the upload form.

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShareImageProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShareImageProject.Controllers
+{
+    public class ImageController : Controller
+    {
+        public IActionResult Upload()
+        {
+            var model = new UploadImageModel();
+            
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult UploadNewImage()
+        {
+            return Ok();
+        }
+    }
+}
